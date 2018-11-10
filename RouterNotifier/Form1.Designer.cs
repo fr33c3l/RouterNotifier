2837509812312bf1fd527bc59692f01a7d83872e
Tomasz Sz <fr33c3l@hotmail.com>
2018-11-10 19:14:42 +0100

    first commit

---
+++ RouterNotifier/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace RouterNotifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon";
+            this.notifyIcon.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1800000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.restartToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(153, 48);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 624);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+    }
+}
+
---
+++ RouterNotifier/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using System.Reflection;
+
+namespace RouterNotifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.Hide();
+            notifyIcon.Icon = new Icon("Icons/" + GetContext() + ".ico");
+
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+        }
+
+
+        private int GetContext()
+        {
+            var client = new RestClient("http://192.168.1.1");
+            client.CookieContainer = new System.Net.CookieContainer();
+            var request = new RestRequest("/authenticate?username=admin&password=", Method.POST);
+            IRestResponse response = client.Execute(request);
+            var content = SimpleJson.DeserializeObject<RootContext>(response.Content);
+            request = new RestRequest("/sysbus/NeMo/Intf/data:getMIBs", Method.POST);
+            request.AddParameter("X-Context", content.data.contextID, ParameterType.HttpHeader);
+
+            response = client.Execute(request);
+            int oMycustomclassname = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(response.Content).status.ppp.ppp_data.LastChange;
+
+            TimeSpan t = TimeSpan.FromSeconds(oMycustomclassname);
+
+            return t.Hours;
+        }
+
+
+
+        public class Context
+        {
+            public string contextID { get; set; }
+        }
+
+        public class RootContext
+        {
+            public int status { get; set; }
+            public Context data { get; set; }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            notifyIcon.Icon = new Icon("Icons/" + GetContext() + ".ico");
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("http://192.168.1.1");
+            client.CookieContainer = new System.Net.CookieContainer();
+            var request = new RestRequest("/authenticate?username=admin&password=", Method.POST);
+            IRestResponse response = client.Execute(request);
+            var content = SimpleJson.DeserializeObject<RootContext>(response.Content);
+            request = new RestRequest("/sysbus/NMC:reboot", Method.POST);
+            request.AddParameter("X-Context", content.data.contextID, ParameterType.HttpHeader);
+            response = client.Execute(request);
+        }
+    }
+}
